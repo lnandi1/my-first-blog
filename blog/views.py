9b794f34167572fa4293df733b6ccd368e792267
LILIAN <lilian_nandi@yahoo.co.uk>
2018-09-15 15:48:40 +0100

    commit

--- blog/views.py
+++ blog/views.py
@@ -6,5 +6,6 @@ from .models import Post
 Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {})
+    print(posts)
+    return render(request, 'blog/post_list.html', {'posts':posts})
 
